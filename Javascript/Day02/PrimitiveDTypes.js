682f1b8dba21df64270db02f3f7f5e203ba2f5f1
Manish Sharma <gautamanesh2469@gmail.com>
2026-07-03 13:07:05 +0530

    Some concepts of number system in js i.e 64 bit SEM

---
+++ Javascript/Day02/PrimitiveDTypes.js
@@ -0,0 +1,33 @@
+// Data types and variables
+//Primitive datatypes (Number, String, Boolean, Big int, NULL, Undefined, Symbol)
+//Number(Int, Floating)
+let accountbal=20;
+let dues = 2000.345;
+console.log(accountbal, ` `, dues);
+console.log(typeof accountbal, typeof dues);
+
+//String
+let comments = "Hello, Queen you look stunning.";
+console.log(comments, typeof(comments));
+
+//Boolean
+let play = false;
+console.log(play, typeof(play));
+
+//Big Int
+let huge = 80000000n;
+console.log(huge, typeof(huge));
+
+//NULL
+let price = null;
+console.log(price, typeof(price));
+
+// Undefined
+let onebyzero = undefined;
+console.log(onebyzero, typeof(onebyzero));
+
+//Symbol
+let x = Symbol(89);
+console.log(x, typeof(x));
+
+console.log(Number.MAX_SAFE_INTEGER);
