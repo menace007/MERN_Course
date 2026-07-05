2a387a58c7b740e3939fca66a34c27f247391a32
Manish Sharma <gautamanesh2469@gmail.com>
2026-07-05 12:16:58 +0530

    Add: Some concepts of comparision operator in js

---
+++ Javascript/Day04/comparision.js
@@ -0,0 +1,30 @@
+// Comparision operator
+//Number to Number
+
+let a1 = 1;
+let a2 = 2;
+console.log(a1>=a2);
+console.log(a1!=a2);//False
+console.log(a1>a2);
+console.log(a1<a2);
+console.log(a1>=a2);
+console.log(a1>=a2);
+console.log(a1>=a2);
+console.log(a1!==a2);
+console.log(a1===a2);
+//null can only be equal to undefined
+console.log(null==undefined);
+console.log(null===undefined);
+console.log(null==0);
+console.log(null<0);
+console.log(null>0);
+console.log(null<=0);
+console.log(null>=0);
+
+//Undefined comparision
+console.log(undefined==0);
+console.log(undefined<0);
+console.log(undefined>0);
+console.log(undefined<=0);
+console.log(undefined>=0);
+console.log(123=="123"==123)
