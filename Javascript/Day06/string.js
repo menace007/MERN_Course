4a22aa4a168cdeba2182e1f7107431d51ec3798e
Manish Sharma <gautamanesh2469@gmail.com>
2026-08-30 10:28:00 +0530

    Basics of string

---
+++ Javascript/Day06/string.js
@@ -0,0 +1,8 @@
+let str = "Hello coder";
+let str1 = " Hello world";
+let str2 = "Hello again";
+console.log(str, str1, str2);
+let price = 100;
+console.log(`Price of tomato is ${price}`);
+// Concatenation
+console.log(str+str1);
