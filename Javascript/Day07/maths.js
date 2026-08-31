640969f1a27f481a97e756cc0ba329ba96a2f3b4
Manish Sharma <gautamanesh2469@gmail.com>
2026-08-31 08:47:26 +0530

    Feat: Add math object basics in js

---
+++ Javascript/Day07/maths.js
@@ -0,0 +1,15 @@
+let num1 = 231;
+let num2 = new Number(231);
+let num3 = new Number(231);
+// console.log(num1)
+// console.log(typeof num2);
+// console.log(num1==num2);
+// console.log(num2==num3);
+// let num = 231.44;
+// console.log(num.toFixed(1));
+// console.log(num.toPrecision(3));
+
+// Math in JS
+// console.log(Math.E);
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 6) + 1);
