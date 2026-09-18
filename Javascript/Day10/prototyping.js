6707165834ed8a53cd275def83ce43eac3f065b1
Manish Sharma <gautamanesh2469@gmail.com>
2026-09-18 11:13:50 +0530

    Add prototype chaining and other concepts

--- Javascript/Day10/prototyping.js
+++ Javascript/Day10/prototyping.js
@@ -7,7 +7,7 @@ let user2 = {
     amount: 20,
     accno: 1234
 }
-user2.__proto__ = user1;
+user2.__proto__ = user1;//Inherit properties from user1 object
 
 console.log(user2.amount);
 console.log(user2.name);
